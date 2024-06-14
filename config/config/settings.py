4c288bbbf32bd79ea01b21ff7af9bad379002733
Nursultanp <chutboyevnursulton96@gmail.com>
2024-06-15 01:12:19 +0600

    Update settings.py

--- config/config/settings.py
+++ config/config/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 # Путь к статическим файлам
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
