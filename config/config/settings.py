6cdad315ecd103ec3b97bfeb68952707ec6b6f3a
Nursultanp <chutboyevnursulton96@gmail.com>
2024-06-14 22:55:59 +0600

    Initial commit

--- config/config/settings.py
+++ config/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s#f1318*v#$%!*72@%qtb7-=u=fa*u^27m_1tg72%@_b7=axyk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['Idiris.com', 'www.Idiris.com']
 
